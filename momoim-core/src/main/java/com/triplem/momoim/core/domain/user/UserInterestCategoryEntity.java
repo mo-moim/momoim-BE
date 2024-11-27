0e7e949370cebffc05f604c146745fb34ae67641
e4g3r <qaws5656@naver.com>
2024-11-27 13:10:12 +0900

    feat: UserInterestCategory Entity 클래스 Column 어노테이션 nullable 옵션 추가

--- momoim-core/src/main/java/com/triplem/momoim/core/domain/user/UserInterestCategoryEntity.java
+++ momoim-core/src/main/java/com/triplem/momoim/core/domain/user/UserInterestCategoryEntity.java
@@ -1,5 +1,6 @@
 package com.triplem.momoim.core.domain.user;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,8 +23,10 @@ public class UserInterestCategoryEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private Long userId;
 
+    @Column(nullable = false)
     private String category;
 
     public static UserInterestCategoryEntity from(UserInterestCategory userInterestCategory) {
