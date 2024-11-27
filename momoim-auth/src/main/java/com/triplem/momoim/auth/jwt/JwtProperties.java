1ab2d6f0cd20bebd1ae383e667e337cd872bffd3
oereo <dlstpgns0406@gmail.com>
2024-11-27 15:38:12 +0900

    feat: jwt 속성에 필요한 정보들을 주입받아 반환하는 역할의 class 정의

---
+++ momoim-auth/src/main/java/com/triplem/momoim/auth/jwt/JwtProperties.java
@@ -0,0 +1,29 @@
+package com.triplem.momoim.auth.jwt;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtProperties {
+
+    private final String accessTokenSecretKey;
+    private final long accessTokenExpireTime;
+
+    public JwtProperties(
+            @Value("${jwt.access-token-secret-key}") String accessTokenSecretKey,
+            @Value("${jwt.access-token-expire-time}") long accessTokenExpireTime
+    ) {
+        this.accessTokenSecretKey = accessTokenSecretKey;
+        this.accessTokenExpireTime = accessTokenExpireTime;
+    }
+
+    protected SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(accessTokenSecretKey.getBytes());
+    }
+
+    protected long getAccessTokenExpireTime() {
+        return accessTokenExpireTime;
+    }
+}
