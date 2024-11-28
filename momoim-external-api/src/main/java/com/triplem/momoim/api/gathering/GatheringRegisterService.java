aa14c02737bff0c45ea1d4496308552a70fbdffd
e4g3r <qaws5656@naver.com>
2024-11-28 21:35:15 +0900

    feat: GatheringRegisterService.register 트랜젝션 처리

--- momoim-external-api/src/main/java/com/triplem/momoim/api/gathering/GatheringRegisterService.java
+++ momoim-external-api/src/main/java/com/triplem/momoim/api/gathering/GatheringRegisterService.java
@@ -5,6 +5,7 @@ import com.triplem.momoim.core.domain.gathering.GatheringRepository;
 import com.triplem.momoim.core.domain.member.GatheringMemberAppender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +13,7 @@ public class GatheringRegisterService {
     private final GatheringRepository gatheringRepository;
     private final GatheringMemberAppender gatheringMemberAppender;
 
+    @Transactional
     public Gathering register(Gathering gathering) {
         Gathering savedGathering = gatheringRepository.save(gathering);
         gatheringMemberAppender.append(savedGathering.getManagerId(), savedGathering.getId());
