c57b5a6bcd8f594e0dc13da2c205c92f703d2c75
e4g3r <qaws5656@naver.com>
2024-11-28 21:06:00 +0900

    refactor: 모임을 등록하는 로직은 현재 단순 저장 로직이므로 GatheringRegister가 아닌 Repository를 직접 이용하도록 변경  - 추후 모임을 등록하는 로직이 복잡해지면 다시 하위 구현체 (GatheringRegister)를 이용하여 처리

--- momoim-external-api/src/main/java/com/triplem/momoim/api/gathering/GatheringRegisterService.java
+++ momoim-external-api/src/main/java/com/triplem/momoim/api/gathering/GatheringRegisterService.java
@@ -1,7 +1,7 @@
 package com.triplem.momoim.api.gathering;
 
 import com.triplem.momoim.core.domain.gathering.Gathering;
-import com.triplem.momoim.core.domain.gathering.GatheringRegister;
+import com.triplem.momoim.core.domain.gathering.GatheringRepository;
 import com.triplem.momoim.core.domain.member.GatheringMemberAppender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class GatheringRegisterService {
-    private final GatheringRegister gatheringRegister;
+    private final GatheringRepository gatheringRepository;
     private final GatheringMemberAppender gatheringMemberAppender;
 
     public Gathering register(Gathering gathering) {
-        Gathering savedGathering = gatheringRegister.register(gathering);
+        Gathering savedGathering = gatheringRepository.save(gathering);
         gatheringMemberAppender.append(savedGathering.getManagerId(), savedGathering.getId());
         return savedGathering;
     }
