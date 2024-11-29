7922577a3f3dbe30da89247c9067e7f2914c1c14
e4g3r <qaws5656@naver.com>
2024-11-29 15:32:18 +0900

    test: GatheringMemberAppenderTest 테스트 코드 GatheringFixture 사용하는 방식으로 변경

--- momoim-core/src/test/java/com/triplem/momoim/core/domain/member/GatheringMemberAppenderTest.java
+++ momoim-core/src/test/java/com/triplem/momoim/core/domain/member/GatheringMemberAppenderTest.java
@@ -4,9 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.triplem.momoim.core.domain.gathering.Gathering;
+import com.triplem.momoim.core.domain.gathering.GatheringFixture;
 import com.triplem.momoim.core.domain.gathering.GatheringRepository;
 import com.triplem.momoim.core.domain.gathering.RecruitStatus;
-import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,25 +26,12 @@ class GatheringMemberAppenderTest {
 
     @Autowired
     private GatheringMemberRepository gatheringMemberRepository;
-
+    
     @Test
     @DisplayName("모임에 멤버를 추가 할 수 있다.")
     void appendGatheringMember() {
         //given
-        LocalDateTime now = LocalDateTime.now();
-        Gathering gathering = gatheringRepository.save(Gathering.builder()
-            .managerId(1L)
-            .category("FOOD")
-            .subCategory("COOK")
-            .recruitStatus(RecruitStatus.RECRUITING)
-            .name("요리 모임")
-            .image("https://placehold.co/600x400")
-            .capacity(10)
-            .participantCount(0)
-            .startAt(LocalDateTime.of(2024, 1, 1, 0, 0))
-            .endAt(LocalDateTime.of(2024, 12, 31, 23, 59))
-            .createdAt(now)
-            .build());
+        Gathering gathering = gatheringRepository.save(GatheringFixture.createGathering(1L, RecruitStatus.RECRUITING, 10, 0));
         Long newMemberUserId = 2L;
 
         //when
@@ -59,27 +46,14 @@ class GatheringMemberAppenderTest {
     @DisplayName("모집 중인 모임이 아니면 멤버를 추가 할 수 없다.")
     void cannotAppendWhenGatheringIsNotRecruiting() {
         //given
-        LocalDateTime now = LocalDateTime.now();
-        Gathering gathering = gatheringRepository.save(Gathering.builder()
-            .managerId(1L)
-            .category("FOOD")
-            .subCategory("COOK")
-            .recruitStatus(RecruitStatus.STOP)
-            .name("요리 모임")
-            .image("https://placehold.co/600x400")
-            .capacity(10)
-            .participantCount(0)
-            .startAt(LocalDateTime.of(2024, 1, 1, 0, 0))
-            .endAt(LocalDateTime.of(2024, 12, 31, 23, 59))
-            .createdAt(now)
-            .build());
+        Gathering notRecruitingGathering = gatheringRepository.save(GatheringFixture.createGathering(1L, RecruitStatus.STOP, 10, 0));
         Long newMemberUserId = 2L;
 
         //when then
-        assertThatThrownBy(() -> gatheringMemberAppender.append(newMemberUserId, gathering.getId()))
+        assertThatThrownBy(() -> gatheringMemberAppender.append(newMemberUserId, notRecruitingGathering.getId()))
             .hasMessageContaining("모집 중인 모임이 아닙니다.");
 
-        Boolean isSuccessAppend = gatheringMemberRepository.isGatheringMember(newMemberUserId, gathering.getId());
+        Boolean isSuccessAppend = gatheringMemberRepository.isGatheringMember(newMemberUserId, notRecruitingGathering.getId());
         assertThat(isSuccessAppend).isFalse();
     }
 
@@ -87,27 +61,14 @@ class GatheringMemberAppenderTest {
     @DisplayName("인원이 다 찬 모임은 멤버를 추가 할 수 없다.")
     void cannotAppendWhenIsFull() {
         //given
-        LocalDateTime now = LocalDateTime.now();
-        Gathering gathering = gatheringRepository.save(Gathering.builder()
-            .managerId(1L)
-            .category("FOOD")
-            .subCategory("COOK")
-            .recruitStatus(RecruitStatus.RECRUITING)
-            .name("요리 모임")
-            .image("https://placehold.co/600x400")
-            .capacity(10)
-            .participantCount(10)
-            .startAt(LocalDateTime.of(2024, 1, 1, 0, 0))
-            .endAt(LocalDateTime.of(2024, 12, 31, 23, 59))
-            .createdAt(now)
-            .build());
+        Gathering fullGathering = gatheringRepository.save(GatheringFixture.createGathering(1L, RecruitStatus.RECRUITING, 10, 10));
         Long newMemberUserId = 2L;
 
         //when then
-        assertThatThrownBy(() -> gatheringMemberAppender.append(newMemberUserId, gathering.getId()))
+        assertThatThrownBy(() -> gatheringMemberAppender.append(newMemberUserId, fullGathering.getId()))
             .hasMessageContaining("인원이 다 찬 모임입니다.");
 
-        Boolean isSuccessAppend = gatheringMemberRepository.isGatheringMember(newMemberUserId, gathering.getId());
+        Boolean isSuccessAppend = gatheringMemberRepository.isGatheringMember(newMemberUserId, fullGathering.getId());
         assertThat(isSuccessAppend).isFalse();
     }
 
@@ -115,20 +76,7 @@ class GatheringMemberAppenderTest {
     @DisplayName("이미 모임에 참가중인 유저는 멤버로 추가 될 수 없다.")
     void cannotAppendWhenIsAlreadyMember() {
         //given
-        LocalDateTime now = LocalDateTime.now();
-        Gathering gathering = gatheringRepository.save(Gathering.builder()
-            .managerId(1L)
-            .category("FOOD")
-            .subCategory("COOK")
-            .recruitStatus(RecruitStatus.RECRUITING)
-            .name("요리 모임")
-            .image("https://placehold.co/600x400")
-            .capacity(10)
-            .participantCount(0)
-            .startAt(LocalDateTime.of(2024, 1, 1, 0, 0))
-            .endAt(LocalDateTime.of(2024, 12, 31, 23, 59))
-            .createdAt(now)
-            .build());
+        Gathering gathering = gatheringRepository.save(GatheringFixture.createGathering(1L, RecruitStatus.RECRUITING, 10, 1));
         GatheringMember member1 = GatheringMember.create(2L, gathering.getId());
         gatheringMemberRepository.save(member1);
 
