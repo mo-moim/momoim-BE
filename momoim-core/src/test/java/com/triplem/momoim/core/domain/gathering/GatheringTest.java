981bc5facc290b35b31ae8e70d408c602fc9a66d
e4g3r <qaws5656@naver.com>
2024-11-30 17:37:16 +0900

    test: GatheringTest 클래스 GatheringBuilder 사용하도록 변경

--- momoim-core/src/test/java/com/triplem/momoim/core/domain/gathering/GatheringTest.java
+++ momoim-core/src/test/java/com/triplem/momoim/core/domain/gathering/GatheringTest.java
@@ -10,7 +10,11 @@ class GatheringTest {
     @DisplayName("isRecruiting 메서드는 모임이 모집중인 상태라면 True를 반환한다.")
     void isRecruiting_returnsTrueWhenGatheringIsRecruiting() {
         //given
-        Gathering recruitingGathering = GatheringFixture.createGathering(1L, RecruitStatus.RECRUITING, 10, 0);
+        Gathering recruitingGathering = GatheringBuilder
+            .builder()
+            .recruitStatus(RecruitStatus.RECRUITING)
+            .build()
+            .toGathering();
 
         //when
         Boolean isRecruiting = recruitingGathering.isRecruiting();
@@ -23,7 +27,11 @@ class GatheringTest {
     @DisplayName("isRecruiting 메서드는 모임이 모집중인 상태가 아니라면 False를 반환한다.")
     void isRecruiting_returnsFalseWhenGatheringIsNotRecruiting() {
         //given
-        Gathering stopGathering = GatheringFixture.createGathering(1L, RecruitStatus.STOP, 10, 0);
+        Gathering stopGathering = GatheringBuilder
+            .builder()
+            .recruitStatus(RecruitStatus.STOP)
+            .build()
+            .toGathering();
 
         //when
         Boolean isRecruiting = stopGathering.isRecruiting();
@@ -36,8 +44,13 @@ class GatheringTest {
     @DisplayName("isFull 메서드는 모집 인원이 다 찼을 경우 True를 반환한다.")
     void isFull_returnsTrueWhenParticipantCountIsFull() {
         //given
-        Gathering fullGathering = GatheringFixture.createGathering(1L, RecruitStatus.RECRUITING, 10, 10);
-
+        Gathering fullGathering = GatheringBuilder
+            .builder()
+            .capacity(10)
+            .participantCount(10)
+            .build()
+            .toGathering();
+        
         //when
         Boolean isFull = fullGathering.isFull();
 
@@ -49,7 +62,11 @@ class GatheringTest {
     @DisplayName("isFull 메서드는 모집 인원에 여유가 있을 경우 False를 반환한다.")
     void isFull_returnsFalseWhenParticipantCountIsNotFull() {
         //given
-        Gathering notFullGathering = GatheringFixture.createGathering(1L, RecruitStatus.RECRUITING, 10, 0);
+        Gathering notFullGathering = GatheringBuilder.builder()
+            .capacity(10)
+            .participantCount(1)
+            .build()
+            .toGathering();
 
         //when
         Boolean isFull = notFullGathering.isFull();
