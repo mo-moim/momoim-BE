325748607cdfd89e585312a509c4d86abab656c2
e4g3r <qaws5656@naver.com>
2024-12-02 17:08:39 +0900

    feat: 방장의 경우 모임 참여 취소를 할 수 없도록 수정

--- momoim-external-api/src/main/java/com/triplem/momoim/api/gathering/GatheringJoinService.java
+++ momoim-external-api/src/main/java/com/triplem/momoim/api/gathering/GatheringJoinService.java
@@ -24,10 +24,14 @@ public class GatheringJoinService {
     public void cancelJoinGathering(Long userId, Long gatheringId) {
         Gathering gathering = gatheringRepository.findById(gatheringId);
 
+        if (gathering.getManagerId().equals(userId)) {
+            throw new RuntimeException("방장은 모임 취소를 할 수 없습니다.");
+        }
+
         if (gathering.isEnd()) {
             throw new RuntimeException("종료 된 모임입니다.");
         }
-
+        
         gatheringMemberRemover.removeGatheringMember(userId, gatheringId);
     }
 }
