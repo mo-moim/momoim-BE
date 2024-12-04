e95413d7185812384814a15f6895ed4ab202c371
e4g3r <qaws5656@naver.com>
2024-12-04 16:46:06 +0900

    feat: Gathering / Review Controller에서 SecurityUtil 사용하도록 변경 및 PreAuthorize 사용

--- momoim-external-api/src/main/java/com/triplem/momoim/api/review/controller/ReviewController.java
+++ momoim-external-api/src/main/java/com/triplem/momoim/api/review/controller/ReviewController.java
@@ -5,6 +5,7 @@ import com.triplem.momoim.api.common.DefaultApiResponse;
 import com.triplem.momoim.api.review.request.ModifyReviewRequest;
 import com.triplem.momoim.api.review.request.RegisterReviewRequest;
 import com.triplem.momoim.api.review.service.ReviewService;
+import com.triplem.momoim.auth.utils.SecurityUtil;
 import com.triplem.momoim.core.common.PaginationInformation;
 import com.triplem.momoim.core.domain.review.Review;
 import com.triplem.momoim.core.domain.review.ReviewDetail;
@@ -13,6 +14,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -36,45 +38,48 @@ public class ReviewController {
     @Operation(operationId = "모임 리뷰 조회", summary = "모임 리뷰 조회", tags = {"reviews"}, description = "모임 리뷰 조회")
     public ApiResponse<List<ReviewDetail>> gatherReview(
         @PathVariable Long gatheringId,
-        @Parameter(name = "userId", description = "인증 설계 완료 전까지 임시로 사용하는 userId") @RequestParam Long userId,
         @Parameter(name = "페이징 offset") @RequestParam int offset,
         @Parameter(name = "페이징 limit") @RequestParam int limit) {
+        Long userId = SecurityUtil.getMemberIdByPrincipal();
         return ApiResponse.success(reviewService.getReviews(gatheringId, userId, new PaginationInformation(offset, limit)));
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('ROLE_USER')")
     @ApiResponses(value = {
         @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청 성공")
     })
     @Operation(operationId = "모임 리뷰 작성", summary = "모임 리뷰 작성", tags = {"reviews"}, description = "모임 리뷰 작성")
     public ApiResponse<Review> registerReview(
-        @Parameter(name = "userId", description = "인증 설계 완료 전까지 임시로 사용하는 userId") @RequestParam Long userId,
         @RequestBody RegisterReviewRequest request) {
+        Long userId = SecurityUtil.getMemberIdByPrincipal();
         Review review = reviewService.register(request.toModel(userId));
         return ApiResponse.success(review);
     }
 
     @PatchMapping("/{reviewId}")
+    @PreAuthorize("hasAnyRole('ROLE_USER')")
     @ApiResponses(value = {
         @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청 성공")
     })
     @Operation(operationId = "모임 리뷰 수정", summary = "모임 리뷰 수정", tags = {"reviews"}, description = "모임 리뷰 수정")
     public DefaultApiResponse modifyReview(
-        @Parameter(name = "userId", description = "인증 설계 완료 전까지 임시로 사용하는 userId") @RequestParam Long userId,
         @Parameter(name = "reviewId", description = "수정 할 리뷰 ID") @PathVariable Long reviewId,
         @RequestBody ModifyReviewRequest request) {
+        Long userId = SecurityUtil.getMemberIdByPrincipal();
         reviewService.modify(request.toModel(reviewId, userId));
         return DefaultApiResponse.success();
     }
 
     @DeleteMapping("/{reviewId}")
+    @PreAuthorize("hasAnyRole('ROLE_USER')")
     @ApiResponses(value = {
         @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청 성공")
     })
     @Operation(operationId = "모임 리뷰 삭제", summary = "모임 리뷰 삭제", tags = {"reviews"}, description = "모임 리뷰 삭제")
     public DefaultApiResponse deleteReview(
-        @Parameter(name = "userId", description = "인증 설계 완료 전까지 임시로 사용하는 userId") @RequestParam Long userId,
         @Parameter(name = "reviewId", description = "삭제 할 리뷰 ID") @PathVariable Long reviewId) {
+        Long userId = SecurityUtil.getMemberIdByPrincipal();
         reviewService.delete(userId, reviewId);
         return DefaultApiResponse.success();
     }
