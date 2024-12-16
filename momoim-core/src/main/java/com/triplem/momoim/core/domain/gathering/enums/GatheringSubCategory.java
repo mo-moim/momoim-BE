f2ee25d0c87eab6b4d0e28bb948921637cb92609
e4g3r <qaws5656@naver.com>
2024-12-16 13:49:27 +0900

    feat: GatheringSubCategory 기획 반영

--- momoim-core/src/main/java/com/triplem/momoim/core/domain/gathering/enums/GatheringSubCategory.java
+++ momoim-core/src/main/java/com/triplem/momoim/core/domain/gathering/enums/GatheringSubCategory.java
@@ -4,19 +4,25 @@ import lombok.Getter;
 
 @Getter
 public enum GatheringSubCategory {
-    MOVIE(GatheringCategory.CULTURE), CONCEPT(GatheringCategory.CULTURE), EXHIBITION(GatheringCategory.CULTURE),
+    MOVIE(GatheringCategory.CULTURE), CONCERT(GatheringCategory.CULTURE), EXHIBITION(GatheringCategory.CULTURE),
+    PERFORMANCE(GatheringCategory.CULTURE),
 
-    RESTAURANT(GatheringCategory.FOOD), COOKING(GatheringCategory.FOOD),
+    RESTAURANT(GatheringCategory.FOOD), COOKING(GatheringCategory.FOOD), BAKING(GatheringCategory.FOOD),
 
-    SOCCER(GatheringCategory.SPORTS), BASKETBALL(GatheringCategory.SPORTS), FUTSAL(GatheringCategory.SPORTS), BIKE(GatheringCategory.SPORTS),
+    SOCCER(GatheringCategory.SPORTS), BASKETBALL(GatheringCategory.SPORTS), BASEBALL(GatheringCategory.SPORTS), BIKE(GatheringCategory.SPORTS),
+    GOLF(GatheringCategory.SPORTS), RUNNING(GatheringCategory.SPORTS), FITNESS(GatheringCategory.SPORTS), DIET(GatheringCategory.SPORTS),
+    YOGA(GatheringCategory.SPORTS), PILATES(GatheringCategory.SPORTS), SWIMMING(GatheringCategory.SPORTS),
 
-    PET(GatheringCategory.HOBBY),
+    PET(GatheringCategory.HOBBY), BOOK(GatheringCategory.HOBBY), MUSIC(GatheringCategory.HOBBY), PHOTO(GatheringCategory.HOBBY),
+    GAME(GatheringCategory.HOBBY), DANCE(GatheringCategory.HOBBY), CRAFTING(GatheringCategory.HOBBY), MEDIA(GatheringCategory.HOBBY),
+    SUB_CULTURE(GatheringCategory.HOBBY),
 
-    HIKING(GatheringCategory.TRAVEL), FISHING(GatheringCategory.TRAVEL), CAMPING(GatheringCategory.TRAVEL),
+    HIKING(GatheringCategory.TRAVEL), FISHING(GatheringCategory.TRAVEL), CAMPING(GatheringCategory.TRAVEL), DOMESTIC(GatheringCategory.TRAVEL),
+    INTERNATIONAL(GatheringCategory.TRAVEL),
 
-    LANGUAGE(GatheringCategory.STUDY), FINANCE(GatheringCategory.STUDY), SELFDEVELOP(GatheringCategory.STUDY),
+    LANGUAGE(GatheringCategory.STUDY), FINANCE(GatheringCategory.STUDY), SELF_DEVELOP(GatheringCategory.STUDY),
 
-    LOVE(GatheringCategory.MEETING), COFFEECHAT(GatheringCategory.MEETING);
+    LOVE(GatheringCategory.MEETING), COFFEE_CHAT(GatheringCategory.MEETING), INTERVIEW(GatheringCategory.MEETING);
     private final GatheringCategory parentCategory;
 
     GatheringSubCategory(GatheringCategory parentCategory) {
