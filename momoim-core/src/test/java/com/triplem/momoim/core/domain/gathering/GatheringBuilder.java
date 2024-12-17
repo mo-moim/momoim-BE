9394a640a1cffcbba86d020bc2d71ca30a0853ff
e4g3r <qaws5656@naver.com>
2024-12-17 17:46:17 +0900

    feat: GatheringType Enum 제거

--- momoim-core/src/test/java/com/triplem/momoim/core/domain/gathering/GatheringBuilder.java
+++ momoim-core/src/test/java/com/triplem/momoim/core/domain/gathering/GatheringBuilder.java
@@ -2,7 +2,6 @@ package com.triplem.momoim.core.domain.gathering;
 
 import com.triplem.momoim.core.domain.gathering.enums.GatheringLocation;
 import com.triplem.momoim.core.domain.gathering.enums.GatheringStatus;
-import com.triplem.momoim.core.domain.gathering.enums.GatheringType;
 import com.triplem.momoim.core.domain.gathering.model.Gathering;
 import java.time.LocalDateTime;
 import java.util.List;
