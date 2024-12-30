6ef13a425ef4001d78fe8e42b3356de3f55cadba
oereo <dlstpgns0406@gmail.com>
2024-12-30 20:46:47 +0900

    feat: cookie domain 재설정

--- momoim-external-api/src/main/java/com/triplem/momoim/api/auth/service/TokenCommandService.java
+++ momoim-external-api/src/main/java/com/triplem/momoim/api/auth/service/TokenCommandService.java
@@ -62,25 +62,13 @@ public class TokenCommandService {
 
         refreshTokenRepository.save(refreshToken);
 
-        for (String domain : DEFAULT_COOKIE_DOMAINS) {
-            if (domain.equals("localhost")) {
-                ResponseCookie cookie = ResponseCookie.from(REFRESH_TOKEN_KEY_IN_COOKIE, tokenInfo.getValue())
-                        .path(COOKIE_DEFAULT_PATH_ROOT)
-                        .httpOnly(true)
-                        .maxAge(DEFAULT_REFRESH_TOKEN_COOKIE_AGE)
-                        .build();
-                response.addHeader(COOKIE_KEY_IN_HEADER, cookie.toString());
-            } else {
-                ResponseCookie cookie = ResponseCookie.from(REFRESH_TOKEN_KEY_IN_COOKIE, tokenInfo.getValue())
-                        .path(COOKIE_DEFAULT_PATH_ROOT)
-                        .domain(domain)
-                        .httpOnly(true)
-                        .maxAge(DEFAULT_REFRESH_TOKEN_COOKIE_AGE)
-                        .secure(true)
-                        .build();
-                response.addHeader(COOKIE_KEY_IN_HEADER, cookie.toString());
-            }
-        }
+        ResponseCookie cookie = ResponseCookie.from(REFRESH_TOKEN_KEY_IN_COOKIE, tokenInfo.getValue())
+                .path(COOKIE_DEFAULT_PATH_ROOT)
+                .domain(cookieDomain)
+                .httpOnly(true)
+                .maxAge(DEFAULT_REFRESH_TOKEN_COOKIE_AGE)
+                .build();
+        response.addHeader(COOKIE_KEY_IN_HEADER, cookie.toString());
     }
 
     public void delete(RefreshToken token) {
@@ -88,11 +76,9 @@ public class TokenCommandService {
     }
 
     public void removeRefreshTokenInCookie(HttpServletResponse response, Cookie refreshToken) {
-        for (String domain : DEFAULT_COOKIE_DOMAINS) {
-            refreshToken.setMaxAge(0);
-            refreshToken.setDomain(domain);
-            refreshToken.setPath("/");
-            response.addCookie(refreshToken);
-        }
+        refreshToken.setMaxAge(0);
+        refreshToken.setDomain(cookieDomain);
+        refreshToken.setPath("/");
+        response.addCookie(refreshToken);
     }
 }
