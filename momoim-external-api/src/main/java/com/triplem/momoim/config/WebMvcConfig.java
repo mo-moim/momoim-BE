fa4661b816e144cef501f5cc81193cd11138ea1a
oereo <dlstpgns0406@gmail.com>
2024-12-30 21:13:33 +0900

    hotfix: addCorsMappings options

--- momoim-external-api/src/main/java/com/triplem/momoim/config/WebMvcConfig.java
+++ momoim-external-api/src/main/java/com/triplem/momoim/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +16,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(gatheringSearchOptionResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://api.momoim.co.kr", "https://www.momoim.co.kr", "https://momoim.co.kr")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .allowedHeaders("*");
     }
 }
